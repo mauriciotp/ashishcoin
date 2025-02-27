9af43ad8fbffd2ea82bdcc1ea425e92f7450fd8a
mauricio <mauricio.tp_@outlook.com>
2025-02-27 19:44:21 -0300

    feat: deploy on sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,9 @@
-import type { HardhatUserConfig } from 'hardhat/config'
+import { vars, type HardhatUserConfig } from 'hardhat/config'
 import '@nomicfoundation/hardhat-toolbox-viem'
+
+const API_KEY = vars.get('API_KEY')
+const INFURA_URL = vars.get('INFURA_URL')
+const WALLET_SECRET = vars.get('WALLET_SECRET')
 
 const config: HardhatUserConfig = {
   solidity: '0.8.28',
@@ -7,11 +11,21 @@ const config: HardhatUserConfig = {
   networks: {
     local: {
       url: 'http://127.0.0.1:8545',
-      chainId: 1337,
+      chainId: 31337,
       accounts: {
         mnemonic: 'test test test test test test test test test test test junk',
       },
     },
+    sepolia: {
+      url: INFURA_URL,
+      chainId: 11155111,
+      accounts: {
+        mnemonic: WALLET_SECRET,
+      },
+    },
+  },
+  etherscan: {
+    apiKey: API_KEY,
   },
 }
 
